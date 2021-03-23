45401ea0a45a09daed5411fadc5a63bffdea62d7
Jean C. Mendoza <73866099+MenJCP@users.noreply.github.com>
2021-03-23 12:31:07 -0400

    Proyect

---
+++ Xamapi/Xamapi/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamapi.Services;
+using Xamapi.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamapi
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new StatePage());
+        }
+
+        protected override void OnStart()
+        {
+            
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Xamapi/Xamapi/Services/IStateApiService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamapi.Models;
+
+namespace Xamapi.Services
+{
+    public interface IStateApiService
+    {
+        Task<StateInfo> GetStateAsync();
+         
+    }
+
+}
